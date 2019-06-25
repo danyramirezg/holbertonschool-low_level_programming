8c709e2976325b4640d606231ea417bada9e61b3
danyramirezg <danyramirezg@gmail.com>
2019-06-25 04:01:09 +0000

    Identify lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
