a986d5d5f4f8a86f563a86d5f486a2b538e472ef
danyramirezg <danyramirez@gmail.com>
2019-06-27 16:16:33 +0000

    Checks uppercase and digit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: integer
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
