e86de8b042c52f2c3dfc7b6840fe4576ce0ba48d
danyramirezg <danyramirezg@gmail.com>
2019-07-01 02:38:36 +0000

    Prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 /**
-* main - Prints all possible different combinations of three digits. 
+* main - Prints all possible different combinations of three digits.
 *
 * Return: void
 */
 int main(void)
 {
-        int a = 0;
-	int d;
-	int c;
-	int b;
-
-        while (a <= 999)
-        {
-                b = (a / 100 + '0');
-                c = (a / 10 % 10 + '0');
-                d = (a % 10 + '0');
-
-                if ((b < c) && (c < d))
-                {
-                        putchar(b);
-                        putchar(c);
-                        putchar(d);
-
-                        if (a != 789)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-                        a++;
-                }
+int a = 0;
+int d;
+int c;
+int b;
+while (a <= 999)
+{
+b = (a / 100 + '0');
+c = (a / 10 % 10 + '0');
+d = (a % 10 + '0');
+if ((b < c)&&(c < d))
+{
+putchar(b);
+putchar(c);
+putchar(d);
+if (a != 789)
+{
+putchar(',');
+putchar(' ');
+}
+}
+a++;
+}
 putchar('\n');
 return (0);
 }
