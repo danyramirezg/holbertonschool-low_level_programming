907bc472fa0695b596ca14d828be7067d3688545
danyramirezg <danyramirezg@gmail.com>
2019-07-05 04:27:47 +0000

    Function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *root13 - Function that encodes a string using rot13
+ * @s: The function's parameter
+ *
+ * Return: Character s.
+ */
+char *rot13(char *s)
+{
+	int x, y;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char boolean;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		boolean = 0;
+		for (y = 0; input[y] != '\0' && boolean == 0; y++)
+		{
+			if (s[x] == input[y])
+			{
+				s[x] = output[y];
+				boolean = 1;
+			}
+		}
+	}
+	return (s);
+}
+
