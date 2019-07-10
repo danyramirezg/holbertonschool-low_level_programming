de567e3a1ebfb71018ebfcb2aee23e3c9d6261d0
danyramirezg <danyramirez@gmail.com>
2019-07-10 21:51:42 +0000

    Task 4

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -16,7 +16,7 @@ else if (n <= 1)
 {
 return (1);
 }
-return (n* factorial(n-1));
+return (n * factorial(n - 1));
 }
 
 
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _pow_recursion - Function that returns the value of x raised to the powe.
+ * @s: The function´s parameter (Character)
+ *
+ * Return: The length of s
+*/
+int _pow_recursion(int x, int y)
