44d64aa67b7aa87f164e75f67724c7fcf22a41ce
danyramirezg <danyramirez@gmail.com>
2019-07-17 01:57:58 +0000

    Task 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr - Concatenates all the arguments of the program
+ * @ac: Argument for the int
+ * @av: Argument for the char
+ *
+ * Return: Always 0.
+ */
+char *argstostr(int ac, char **av)
+{
+}
