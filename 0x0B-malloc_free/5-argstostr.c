125f8d69659780c22f824d9e8a0eb44036e435a8
danyramirezg <danyramirezg@gmail.com>
2019-07-18 01:23:40 +0000

    Function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -11,4 +11,31 @@
  */
 char *argstostr(int ac, char **av)
 {
+int i, j, siz, temp;
+char *p;
+
+	if (av == NULL)
+		return (NULL);
+	if (ac == 0)
+		return (NULL);
+
+	i = j = siz = temp = 0;
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			siz++;
+	}
+	p = (char *)malloc(siz * sizeof(char) + ac + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++, temp++)
+			p[temp] = av[i][j];
+		p[temp] = '\n';
+		temp++;
+	}
+	p[temp] = 0;
+	return (p);
+
 }
