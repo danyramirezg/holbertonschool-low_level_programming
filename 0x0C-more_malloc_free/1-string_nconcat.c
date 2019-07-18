7d384c2b634a0f40748e00902435136b89ec275c
danyramirezg <danyramirez@gmail.com>
2019-07-18 23:54:39 +0000

    Fix tax 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * size - To find the size of the string
+ /**
+ * size - Find the size of the string
  * @str: Argument for the char
  *
- * Return: The pointer - result.
+ * Return: i.
  */
 
-	unsigned int size(char *str)
+unsigned int size(char *str)
 	{
 	unsigned int i;
 
@@ -25,19 +25,20 @@
 	}
 	return (i);
 }
+
+ /**
+ * *string_nconcat - Concatenates two strings
+ * @s1: String 1
+ * @s2: string 2
+ * @n: Unsigned integer
+ *
+ * Return: The the result
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int size1, size2, i, j;
 	char *result;
 
-	size1 = size(s1);
-	size2 = size(s2);
-	result = malloc(size1 + size2 + 1);
-
-if (n >= size2)
-{
-n = size2;
-}
 if (s1 == NULL)
 {
 s1 = "";
@@ -45,6 +46,15 @@ s1 = "";
 if (s2 == NULL)
 {
 s2 = "";
+}
+
+size1 = size(s1);
+size2 = size(s2);
+result = malloc(size1 + size2 + 1);
+
+if (n >= size2)
+{
+n = size2;
 }
 if (result == NULL)
 {
