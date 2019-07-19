35b7bc12e2faf2d6211ee595b0375fcbc5068d61
danyramirezg <danyramirezg@gmail.com>
2019-07-19 02:23:44 +0000

    Fix output and style

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,9 +20,20 @@ int rango, i;
 	p = malloc((rango) * sizeof(*p));
 	if (p == NULL)
 	return (NULL);
-  for (i = 0; i <= rango; min++, i++)
-    {
-      p[i] = min;
-    }
-	return (p);
+	for (i = 0; i <= rango; min++, i++)
+	{
+		p[i] = min;
+	}
+return (p);
+}
+
+/**
+  * _abs - Get absolute value
+  * @n: number to calculate
+  * Return: absolute value of the number
+  */
+int _abs(int n)
+{
+	n < 0 ? (n *= -1) : (n = n);
+	return (n);
 }
