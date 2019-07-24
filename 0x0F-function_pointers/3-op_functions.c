91d40a285fb89e2ea1cb59902f9f8083d7d7f3e0
danyramirezg <danyramirez@gmail.com>
2019-07-24 22:50:30 +0000

    First files task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * int_index - Function that searches for an integer
+ * @array: Argument for the integer
+ * @size: Number of elements in the array
+ * @cmp: Pointer to the function to compare values
+ * Return: The function´s pointer
+ */
+int (*get_op_func(char *s))(int, int)
+
