301e11d25a98103611bc34f2daa17c55af108460
danyramirezg <danyramirezg@gmail.com>
2019-07-25 04:14:45 +0000

    Correction outputs

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,7 +10,6 @@
   */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
 op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
@@ -19,7 +18,7 @@ op_t ops[] = {
 	{"%", op_mod},
 	{NULL, NULL}
 };
-	i = 0;
+	int i = 0;
 	while (i < 5)
 	{
 		if (*ops[i].op == *s && !(*(s + 1)))
