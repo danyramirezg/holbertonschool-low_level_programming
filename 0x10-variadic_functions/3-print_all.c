88ca69dc2d26b5ec9a1dde05262d78be2acaecb6
danyramirezg <danyramirezg@gmail.com>
2019-07-28 15:15:55 +0000

    Fix task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,6 +14,46 @@ bool is_ok(const char fmt)
 	return (fmt == 'c' || fmt == 'i' || fmt == 'f' || fmt == 's');
 }
 
+const char* move_next(const char* fmt) 
+{
+	while (*fmt != '\0' && !is_ok(*fmt)) 
+	{
+	fmt++;
+  	}
+	return (fmt);
+}
+
+const char* print_and_advance(const char* fmt, va_list args) 
+{
+	int i;
+	double f;
+	char* str;
+	switch (*fmt) 
+	{
+	case 'i':
+	i = va_arg(args, int);
+	printf("%d", i);
+	break;
+	case 'c':  
+	i = va_arg(args, int);
+	printf("%c", i);
+	break;
+	case 'f':
+	f = va_arg(args, double);
+	printf("%f", f);
+	break;
+	case 's':
+	str = va_arg(args, char*);
+	if (str == NULL) 
+{
+	printf("(nil)");
+	break;
+} 
+	printf("%s", str);
+	break;
+}
+	return ++fmt;
+}
 /**
  * print_all - Function that prints anything
  * @format: A list of types of arguments
@@ -23,54 +63,21 @@ bool is_ok(const char fmt)
 
 void print_all(const char * const format, ...) 
 {
-	int i;
-	double f;
-	char* str;
-	const char* fmt = format;
-	int first = 1;
-	va_list args;
-	va_start(args, format);
- 
-	while (*fmt != '\0' && (is_ok(*fmt) || fmt++))
-	{
-        if (!first && is_ok(*fmt))
-	{ 
-		printf(", ");
-        } 
-        while (first)
-	{
-		first = 0;
-	}
-	switch (*fmt)
-	{
-	case 'i':
-	i = va_arg(args, int);
-		printf("%d", i);
-		fmt++;
-		continue;
-	case 'c':
-	i = va_arg(args, int);
-		printf("%c", i);
-		fmt++;
-		continue;
-          case 'f':
-            f = va_arg(args, double);
-            printf("%f", f);
-            fmt++;
-            continue;
-          case 's':
-            str = va_arg(args, char*);
-            if (str == NULL) {
-              printf("(nil)");
-              fmt++;
-              continue;
-            } 
-            printf("%s", str);
-            fmt++;
-            continue;
-        }
-    }
- 
-    va_end(args);
+
+const char* fmt = format;
+va_list args;
+va_start(args, format);
+
+fmt = move_next(fmt);
+if (*fmt != '\0') 
+{
+fmt = print_and_advance(fmt, args);
+}
+while (*(fmt = move_next(fmt)) != '\0') 
+{
+printf(", ");
+fmt = print_and_advance(fmt, args);
+} 
+va_end(args);
 putchar(10);
 }
