f56d733b211118f92b49882ea78351f733e192b8
danyramirezg <danyramirezg@gmail.com>
2019-07-28 18:18:55 +0000

    Fix task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,86 +1,69 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
+#include <stdbool.h>
 
-/**
- * move_next - To through the string.
- * @fmt: The function's parameter
- *
- * Return: The format
- */
-const char *move_next(const char *fmt)
-{
-
-while (*fmt != '\0' && !(*fmt == 'c' || *fmt == 'i'
-	|| *fmt == 'f' || *fmt == 's'))
-{
-	fmt++;
-	}
-	return (fmt);
+bool is_ok(const char fmt) {
+  return fmt == 'c' || fmt == 'i' || fmt == 'f' || fmt == 's';
 }
 
-/**
- * print_and_advance - Function that prints
- * @fmt: The format.
- * @args: The list's arguments
- *
- * Return: The formate.
- */
-const char *print_and_advance(const char *fmt, va_list args)
-{
-	char *str;
-	
-	switch (*fmt)
-	{
-	case 'i':
-	printf("%d",(int) va_arg(args, int));
-	break;
-	case 'c':
-	printf("%c", (char) va_arg(args, int));
-	break;
-	case 'f':
-printf("%f", (double) va_arg(args, double));
-  break;
-  case 's':
-  str = va_arg(args, char*);
-  if (str == NULL)
-  {
-  printf("(nil)");
-  break;
+const char* move_next(const char* fmt) {
+  while (*fmt != '\0' && !is_ok(*fmt)) {
+    fmt++;
   }
-  printf("%s", str);
-  break;
+  return fmt;
 }
 
-
-  return (++fmt);
+const char* print_and_advance(const char* fmt, va_list args) {
+    int i;
+    double f;
+    char* str;
+    switch (*fmt) {
+      case 'i':
+        i = va_arg(args, int);
+        printf("%d", i);
+        break;
+      case 'c':  
+        i = va_arg(args, int);
+        printf("%c", i);
+        break;
+      case 'f':
+        f = va_arg(args, double);
+        printf("%f", f);
+        break;
+      case 's':
+        str = va_arg(args, char*);
+        if (str == NULL) {
+          printf("(nil)");
+          break;
+        } 
+        printf("%s", str);
+        break;
+    }
+    return ++fmt;
 }
+/*
+where format is a list of types of arguments passed to the function
+c: char
+i: integer
+f: float
+s: char * (if the string is NULL, print (nil) instead
+any other char should be ignored
+see example
+*/
+void print_all(const char * const format, ...) {
+    const char* fmt = format;
+    va_list args;
+    va_start(args, format);
 
-/**
- * print_all - Function that prints anything
- * @format: A list of types of arguments
- *
- * Return: Nothing
- */
+    fmt = move_next(fmt);
+    if (*fmt != '\0') {
+      fmt = print_and_advance(fmt, args);
+    }
+    while (*(fmt = move_next(fmt)) != '\0') {
+        printf(", ");
+        fmt = print_and_advance(fmt, args);
+    }
 
-void print_all(const char * const format, ...)
-{
-	const char *fmt = format;
-	va_list args;
-
-	va_start(args, format);
-
-fmt = move_next(fmt);
-if (*fmt != '\0')
-{
-fmt = print_and_advance(fmt, args);
-}
-while (*(fmt = move_next(fmt)) != '\0')
-{
-printf(", ");
-fmt = print_and_advance(fmt, args);
-}
-va_end(args);
+    va_end(args);
 putchar(10);
 }
