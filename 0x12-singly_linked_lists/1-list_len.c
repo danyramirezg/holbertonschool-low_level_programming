da84cde0689a68a0a8642823578ad197010ba522
danyramirezg <danyramirezg@gmail.com>
2019-08-02 00:09:28 +0000

    Fix Betty style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,18 +4,20 @@
 
 /**
 * list_len - Function that returns the number of elements in a linked list.
-*@h: Receive the list to print
-*Return: The number of nodes
+* @h: Receive the list to print
+*
+* Return: The number of nodes
 */
+
 size_t list_len(const list_t *h)
 {
 	size_t i;
-	i = 0;
 
+	i = 0;
 	while (h != NULL)
-	{	
+	{
 		h = h->next;
 		i++;
-	}	
+	}
 	return (i);
 }
