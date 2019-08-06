aaed4cdeff6170e025df78e9fad7ee10359ed574
danyramirezg <danyramirez@gmail.com>
2019-08-06 15:58:51 +0000

    Fix Betty style

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
@@ -29,6 +29,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);||:
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
