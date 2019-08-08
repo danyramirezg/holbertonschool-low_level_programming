75f3b75ca8dc6d69cf743c35bb34a896eaf93268
danyramirezg <danyramirez@gmail.com>
2019-08-08 22:24:13 +0000

    Fix output and style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,20 +4,20 @@
 /**
 * print_binary - Prints the binary representation of a number
 * @n: Unsigned int
-* Return: Nothing 
+* Return: Nothing
 */
 void print_binary(unsigned long int n)
 {
-	int i = 0, size = 0, var;
+	unsigned int i = 0, size = 0, var;
 
 	if (n == 0)
 		_putchar('0');
 
-	while(n >> size)
+	while (n >> size)
 	{
 		size++;
 	}
-	for (i = (size - 1); i >= 0; i --)
+	for (i = (size - 1); i >= 0; i--)
 	{
 		var = n >> i;
 
