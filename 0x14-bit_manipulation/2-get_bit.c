2539d62e19f89e85e76d105fc8f54f9eed5d398a
danyramirezg <danyramirez@gmail.com>
2019-08-09 00:01:48 +0000

    Fix style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 
 /**
-* get_bit - Returns the value of a bit 
+* get_bit - Returns the value of a bit
 * @n: Unsigned long int
 * @index: Value given
 * Return: The value of the bit
@@ -16,9 +16,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	n >>= index;
 
 	if ((n & 1) == 1)
-		return(1);
-	else
-	{
-	return(0);
-	}
+		return (1);
+
+	return (0);
 }
