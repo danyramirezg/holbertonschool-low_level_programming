02322f15fc1a8c94662679cc9c8e7a6c1ef8e891
danyramirezg <danyramirezg@gmail.com>
2019-08-13 21:16:33 +0000

    Fix Betty Style

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	for(len = 0; text_content[len]; len ++)
+	for (len = 0; text_content[len]; len++)
 		;
 	write_f = write(append, text_content, len);
 	close(append);
