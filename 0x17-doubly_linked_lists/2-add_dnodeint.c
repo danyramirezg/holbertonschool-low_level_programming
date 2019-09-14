91de6fb0cf84ea7d2e69872c2cbf4a9c16585d14
Dany <danyramirezg@gmail.com>
2019-09-14 17:55:08 -0500

    Delete unused line

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	new_node = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
