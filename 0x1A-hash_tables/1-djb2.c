db4771c3c493edf0266663b79166e491bb87341e
Dany <danyramirezg@gmail.com>
2019-10-10 23:02:42 -0500

    hash function implementing the djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Function implementing the djb2 algorithm.
+ * @str: String
+ *
+ * Return: hash.
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	Unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
