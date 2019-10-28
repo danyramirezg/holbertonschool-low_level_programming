b71ca72ad9061e746e338aa405e3141cd87a4c33
danyramirezg <danyramirezg@gmail.com>
2019-10-28 15:25:27 +0000

    Correct output

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * buble_sort - sorts an array of integers in ascending order
+ * @array: Array of integers to sort
+ * @size: The size of the array to be sorted
+ * Return: Sorted array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, temp;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
