61664915b496f0898fe10a9273e0aaa458e81854
Dany <danyramirezg@gmail.com>
2019-12-03 11:52:29 -0500

    Add Readme and header

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: Pointer to the structure
+ * @value: Integer
+ * Return: A binary tree node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
