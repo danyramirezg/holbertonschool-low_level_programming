31881dff546493d9c413138c7a7c2a22d3c67ea1
Dany <danyramirezg@gmail.com>
2019-12-04 12:31:15 -0500

    function that checks if a node is a leaf

---
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ *
+ * Return: A binary tree node
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+}
+
