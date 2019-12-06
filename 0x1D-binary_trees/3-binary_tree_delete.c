d8db2b6dd00f0a6fa9c4232c273191a5a518b8a8
Dany <danyramirezg@gmail.com>
2019-12-06 14:52:40 -0500

    Function that deletes an entire binary tree

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -13,5 +13,13 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
+	if (tree->left)
+	{
+		binary_tree_delete(tree->left);
+	}
+	if (tree->right)
+	{
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
 }
-
