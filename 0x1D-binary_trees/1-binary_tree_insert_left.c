680609a2f8b347fe99e6ab3be87e3b8ff35bdd99
Dany <danyramirezg@gmail.com>
2019-12-06 14:35:46 -0500

    Add condition when parent is NULL

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -14,6 +14,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *new_left, *aux;
 
+	if (parent == NULL)
+		return NULL;
+
 	new_left = malloc(sizeof(binary_tree_t));
 	if (new_left == NULL)
 		return (NULL);
